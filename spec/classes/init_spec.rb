03d3d16025b4baf37beb89cf67271ea70caa389a
Tim Meusel <tim@bastelfreak.de>
2022-04-01 20:09:45 +0200

    Download binary, manage systemd unit file

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -11,6 +11,11 @@ describe 'matterbridge' do
 
       context 'with all defaults' do
         it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_systemd__unit_file('matterbridge.service') }
+        it { is_expected.to contain_user('matterbridge') }
+        it { is_expected.to contain_group('matterbridge') }
+        it { is_expected.to contain_file('/usr/local/bin/matterbridge').with_ensure('link') }
+        it { is_expected.to contain_file('/opt/matterbridge/matterbridge-1.24.1-linux-64bit') }
       end
     end
   end
