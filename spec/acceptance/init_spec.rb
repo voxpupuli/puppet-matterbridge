b8c44ec1c785bc492f878d84332d960b646d85f8
Tim Meusel <tim@bastelfreak.de>
2022-04-04 13:35:32 +0200

    Enhance acceptance tests

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -3,10 +3,29 @@
 require 'spec_helper_acceptance'
 
 describe 'matterbridge' do
-  context 'with defaults' do
+  context 'with minimal config' do
     it_behaves_like 'an idempotent resource' do
       let(:manifest) do
-        'include matterbridge'
+        <<-PUPPET
+        class { 'matterbridge':
+          config => {
+            'irc' => {
+              'libera' => {
+                'Server' => 'irc.libera.chat:6667',
+                'Nick' => 'test',
+              },
+            },
+            'gateway' => {
+              'name' => 'foo',
+              'enable' => true,
+              'inout' => {
+                'account' => 'irc.libera',
+                'channel' => '#testing',
+              },
+            },
+          },
+        }
+        PUPPET
       end
     end
     describe service('matterbridge') do
